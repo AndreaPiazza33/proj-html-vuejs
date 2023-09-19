25faa62143f93e200f79052516a17e2e9349d39b
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-09-19 18:17:57 +0200

    feat: add header layout structure

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,6 +8,54 @@ export default {
 };
 </script>
 
-<template></template>
+<template>
+  <div class="bg-header">
+    <!--contenitore header-->
+    <div class="container">
+      <div class="d-flex flex-row justify-content-between">
+        <!--logo-->
+        <div>
+          <img src="img/dark-logo.png" alt="" />
+        </div>
+        <!--navbar-->
+        <div class="d-flex flex-row align-items-center">
+          <!--options-->
+          <div>
+            <ul class="d-flex flex-row">
+              <li>link</li>
+              <li>link</li>
+              <li>link</li>
+              <li>link</li>
+              <li>link</li>
+              <li>link</li>
+            </ul>
+          </div>
+          <!--icone-->
+          <div class="d-flex flex-row align-items-center mx-2">
+            <p>icon</p>
+            <p>icon</p>
+          </div>
+          <!--search-->
+          <div>
+            <input type="text" />
+            <button>search</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.bg-header {
+  background-color: rgb(233 230 227);
+  width: 100%;
+  ul {
+    gap: 1rem;
+    list-style-type: none;
+  }
+  img {
+    max-width: 100%;
+  }
+}
+</style>
